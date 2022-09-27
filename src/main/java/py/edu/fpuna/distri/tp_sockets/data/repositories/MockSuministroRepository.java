3b995c246988608f7415c18252d94c0906a9d203
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-09-27 16:30:51 -0400

    Creamos el repositorio para la BD Local que usaremos con el metodo de registrar consumo del usuario. Agregamos el test del repositorio

---
+++ src/main/java/py/edu/fpuna/distri/tp_sockets/data/repositories/MockSuministroRepository.java
@@ -0,0 +1,61 @@
+package py.edu.fpuna.distri.tp_sockets.data.repositories;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import py.edu.fpuna.distri.tp_sockets.domain.entities.Suministro;
+import py.edu.fpuna.distri.tp_sockets.domain.repositories.SuministroRepository;
+
+public class MockSuministroRepository implements SuministroRepository {
+    private final Map<String, Suministro> bdLocal;
+
+    public MockSuministroRepository(Map<String, Suministro> bdLocal) {
+        this.bdLocal = bdLocal;
+    }
+
+    private Suministro getSuministro(String nis) {
+        return bdLocal.get(nis);
+    }
+
+    @Override
+    public Suministro registrarConsumo(String nis) {
+        Suministro suministro = getSuministro(nis);
+        if (suministro == null) {
+            return null;
+        }
+        suministro.setConsumo(suministro.getConsumo());
+        return suministro;
+    }
+
+    @Override
+    public void verificarConectividad(String nis) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void enviarOrdenDesconexion(String nis) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void enviarOrdenConexion(String nis) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public List<Suministro> listarSuministrosActivos() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Suministro> listarSuministrosInactivos() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ src/test/java/py/edu/fpuna/distri/tp_sockets/data/repositories/MockSuministroRepositoryTest.java
@@ -0,0 +1,51 @@
+package py.edu.fpuna.distri.tp_sockets.data.repositories;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import py.edu.fpuna.distri.tp_sockets.domain.entities.Suministro;
+import py.edu.fpuna.distri.tp_sockets.domain.entities.EstadoActual;
+
+import py.edu.fpuna.distri.tp_sockets.domain.entities.Suministro;
+import py.edu.fpuna.distri.tp_sockets.domain.repositories.SuministroRepository;
+
+public class MockSuministroRepositoryTest {
+
+    @Test
+    public void registrarConsumoDeberiaRetornarNullEnCasoNoExiste() {
+        Map<String, Suministro> bdLocal = new HashMap<>();
+        SuministroRepository mockSuministroRepository = new MockSuministroRepository(bdLocal);
+
+        Suministro suministro = mockSuministroRepository.registrarConsumo("123456789");
+
+        assertNull(suministro);
+    }
+
+    @Test
+    public void registrarConsumoDeberiaRetornarSuministro() {
+        Suministro suministroSeleccionado = new Suministro("123456789", "Juan Perez", 100, 100.0, EstadoActual.ACTIVO);
+        List<Suministro> suministros = new ArrayList<>();
+        suministros.add(suministroSeleccionado);
+        suministros.add(new Suministro("987654323421", "Maria Lopez", 234, 200, EstadoActual.ACTIVO));
+        suministros.add(new Suministro("15423456789", "Juan Perez", 54353, 100, EstadoActual.ACTIVO));
+        suministros.add(new Suministro("98743534654321", "Maria Lopez", 345543, 200, EstadoActual.ACTIVO));
+
+        Map<String, Suministro> bdLocal = new HashMap<>();
+        for (Suministro suministro : suministros) {
+            bdLocal.put(suministro.getNis(), suministro);
+        }
+
+        SuministroRepository mockSuministroRepository = new MockSuministroRepository(bdLocal);
+
+        Suministro suministro = mockSuministroRepository.registrarConsumo("123456789");
+
+        assertEquals(suministro, suministroSeleccionado);
+    }
+
+}
