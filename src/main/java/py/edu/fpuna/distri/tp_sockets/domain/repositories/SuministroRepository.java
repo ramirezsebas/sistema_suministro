5e9255897133bcf4968a7f1428fbba2a41343f52
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-09-27 14:11:16 -0400

    Creamos un repositorio donde definimos todas funcionalidades

---
+++ src/main/java/py/edu/fpuna/distri/tp_sockets/domain/repositories/SuministroRepository.java
@@ -0,0 +1,20 @@
+package py.edu.fpuna.distri.tp_sockets.domain.repositories;
+
+import java.util.List;
+
+import py.edu.fpuna.distri.tp_sockets.domain.entities.Suministro;
+
+public interface SuministroRepository {
+    public void registrarConsumo(String nis);
+
+    public void verificarConectividad(String nis);
+
+    public void enviarOrdenDesconexion(String nis);
+
+    public void enviarOrdenConexion(String nis);
+
+    public List<Suministro> listarSuministrosActivos();
+
+    public List<Suministro> listarSuministrosInactivos();
+
+}
