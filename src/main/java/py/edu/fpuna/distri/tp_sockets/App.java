1c72852c22286142858097a3585133a99cc768ee
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-09-27 13:43:48 -0400

    Iniciamos un proyecto con maven

---
+++ src/main/java/py/edu/fpuna/distri/tp_sockets/App.java
@@ -0,0 +1,11 @@
+package py.edu.fpuna.distri.tp_sockets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
